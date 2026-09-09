2be5eadff51988ebae69c1270a5492a672112437
Ilya Shelkunov <shelkunow48@gmail.com>
2026-09-09 12:09:40 +0300

    feat:implement favorites schema and toggleFavorite server action

---
+++ src/db/actions.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { and, eq } from "drizzle-orm"
+import { db } from "."
+import { favorites } from "./schema"
+import { revalidatePath } from "next/cache";
+
+export async function toggleFavorite(toolId: string) {
+    const existingFavorite = await db.select().from(favorites).where(and(
+        eq(favorites.toolId, toolId),
+        eq(favorites.userId, "system_user")
+    ));
+    if (existingFavorite.length > 0) {
+        await db.delete(favorites).where(eq(favorites.id, existingFavorite[0].id))
+    } else {
+        await db.insert(favorites).values({
+            toolId,
+            userId: "system_user"
+        });
+    }
+}
+revalidatePath('/');
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 export const aiTools = pgTable('ai_tools', {
     id: uuid("id").defaultRandom().primaryKey(),
@@ -8,3 +8,10 @@ export const aiTools = pgTable('ai_tools', {
     category: text('category').notNull(),
     createdAt: timestamp('createdAt').defaultNow()
 });
+export const favorites = pgTable("favorites", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    toolId: uuid("tool_id").references(() => aiTools.id).notNull(),
+    userId: text("user_id").default("system_user").notNull(),
+}, (table) => ({
+    uniqueIdx: uniqueIndex("unique_favorite").on(table.toolId, table.userId),
+}));
