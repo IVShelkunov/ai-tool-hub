2ce999245c92c11d2f61d541aacd10bac349c8f7
Ilya Shelkunov <shelkunow48@gmail.com>
2026-09-17 13:04:37 +0300

    feat: add users verification schema and isVerified field

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,27 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
-
+import { boolean, pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+//users
 export const users = pgTable('users', {
     id: uuid("id").defaultRandom().primaryKey(),
     email: text("email").notNull().unique(),
-    password: text("password").notNull()
+    password: text("password").notNull(),
+    isVerified: boolean("is_verified").default(false).notNull()
 });
+export const verificationTokens = pgTable("verification_tokens", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    token: text("token").notNull().unique(),
+    userId: uuid("user_id").references(() => users.id).notNull(),
+    expiresAt: timestamp("expires_at", { withTimezone: true, mode: 'date' }).notNull()
+});
+
+export const sessions = pgTable('sessions', {
+    id: uuid("id").defaultRandom().primaryKey(),
+    sessionToken: text("session_token").notNull().unique(),
+    userId: uuid("user_id").references(() => users.id).notNull(),
+    expiresAt: timestamp("expires_at", { withTimezone: true, mode: 'date' }).notNull()
+});
+
+//tools
 export const aiTools = pgTable('ai_tools', {
     id: uuid("id").defaultRandom().primaryKey(),
     name: text('name').notNull(),
@@ -14,6 +30,7 @@ export const aiTools = pgTable('ai_tools', {
     category: text('category').notNull(),
     createdAt: timestamp('createdAt').defaultNow()
 });
+
 export const favorites = pgTable('favorites', {
     id: uuid("id").defaultRandom().primaryKey(),
     toolId: uuid("tool_id").references(() => aiTools.id).notNull(),
@@ -21,12 +38,7 @@ export const favorites = pgTable('favorites', {
 }, (table) => ({
     uniqueIdx: uniqueIndex("unique_favorite").on(table.toolId, table.userId)
 }));
-export const sessions = pgTable('sessions', {
-    id: uuid("id").defaultRandom().primaryKey(),
-    sessionToken: text("session_token").notNull().unique(),
-    userId: uuid("user_id").references(() => users.id).notNull(),
-    expiresAt: timestamp("expires_at", { withTimezone: true, mode: 'date' }).notNull()
-});
+
 
 export const userRelation = relations(users, ({ many }) => ({
     favorites: many(favorites)
